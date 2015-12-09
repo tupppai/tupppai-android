47dcb2fbf65f8875461e29ba35b439253d115d37
againflan <againflan@yahoo.com>
2015-12-09 18:36:59 +0800

    修复bug

--- appStartActivity/src/main/java/com/psgod/ui/activity/LoginActivity.java
+++ appStartActivity/src/main/java/com/psgod/ui/activity/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Looper;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -382,12 +383,14 @@ public class LoginActivity extends PSGodBaseActivity implements
 				weixin.setPlatformActionListener(new PlatformActionListener() {
 					@Override
 					public void onError(Platform arg0, int arg1, Throwable arg2) {
+						Looper.prepare();
 						if (mProgressDialog != null
 								&& mProgressDialog.isShowing()) {
 							mProgressDialog.dismiss();
 						}
 						CustomToast.show(LoginActivity.this, "微信登录异常",
 								Toast.LENGTH_LONG);
+						Looper.loop();
 					}
 
 					@Override
