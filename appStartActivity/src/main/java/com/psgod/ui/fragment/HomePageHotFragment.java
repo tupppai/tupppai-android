f2c643fdaf3a38cf130a364ea0559da33c9ed2a3
pires <1764840217@qq.com>
2015-12-21 11:10:00 +0800

    首页banner重构

--- appStartActivity/src/main/java/com/psgod/ui/fragment/HomePageHotFragment.java
+++ appStartActivity/src/main/java/com/psgod/ui/fragment/HomePageHotFragment.java
@@ -12,13 +12,15 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler.Callback;
 import android.os.Message;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
-import android.widget.ExpandableListView;
-import android.widget.ExpandableListView.OnGroupClickListener;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 import com.android.volley.RequestQueue;
@@ -28,11 +30,13 @@ import com.android.volley.VolleyError;
 import com.handmark.pulltorefresh.library.PullToRefreshBase;
 import com.handmark.pulltorefresh.library.PullToRefreshBase.OnLastItemVisibleListener;
 import com.handmark.pulltorefresh.library.PullToRefreshBase.OnRefreshListener;
-import com.handmark.pulltorefresh.library.PullToRefreshExpandableListView;
+import com.handmark.pulltorefresh.library.PullToRefreshListView;
 import com.j256.ormlite.android.apptools.OpenHelperManager;
 import com.j256.ormlite.dao.Dao;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.psgod.Constants;
 import com.psgod.Logger;
+import com.psgod.PsGodImageLoader;
 import com.psgod.R;
 import com.psgod.ThreadManager;
 import com.psgod.WeakReferenceHandler;
@@ -43,12 +47,16 @@ import com.psgod.model.BannerData;
 import com.psgod.model.Comment;
 import com.psgod.model.PhotoItem;
 import com.psgod.network.NetworkUtil;
+import com.psgod.network.request.BaseRequest;
 import com.psgod.network.request.HomePageGetBannerRequest;
 import com.psgod.network.request.PSGodErrorListener;
 import com.psgod.network.request.PSGodRequestQueue;
 import com.psgod.network.request.PhotoListRequest;
+import com.psgod.ui.activity.ChannelActivity;
 import com.psgod.ui.activity.CommentListActivity;
-import com.psgod.ui.adapter.HomePageHotAdapter;
+import com.psgod.ui.activity.RecentActActivity;
+import com.psgod.ui.activity.WebBrowserActivity;
+import com.psgod.ui.adapter.PhotoListAdapter;
 import com.psgod.ui.view.PhotoItemView;
 import com.psgod.ui.widget.EditPopupWindow;
 import com.psgod.ui.widget.EditPopupWindow.OnResponseListener;
@@ -59,7 +67,9 @@ import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import cn.trinea.android.view.autoscrollviewpager.AutoScrollViewPager;
 import de.greenrobot.event.EventBus;
+import m.framework.utils.Utils;
 
 public class HomePageHotFragment extends BaseFragment implements Callback {
     private static final String TAG = HomePageHotFragment.class.getSimpleName();
@@ -73,7 +83,7 @@ public class HomePageHotFragment extends BaseFragment implements Callback {
     private ExecutorService fixedThreadPool = Executors.newFixedThreadPool(1);
 
     // 带评论
-    private HomePageHotAdapter mAdapter;
+    private PhotoListAdapter mAdapter;
     private PhotoListListener mListener;
     private View mFollowListFooter;
     private int mPage = 1;
@@ -87,15 +97,22 @@ public class HomePageHotFragment extends BaseFragment implements Callback {
     private boolean canLoadMore = true;
 
     private EditPopupWindow editWindow;
+
     //  显示的banner
-    private List<BannerData> bannerList = new ArrayList<BannerData>();
+    private List<BannerData> mBannerItems = new ArrayList<BannerData>();
+    private View bannerView;
+    private AutoScrollViewPager mBannerViewPager;
+    private BannerOnPageChangeListener bannerListener = new BannerOnPageChangeListener();
+    private List<ImageView> mScrollImages = new ArrayList<ImageView> ();
+
+    private LinearLayout mScrollLayout;
+    private DisplayImageOptions mOptions = Constants.DISPLAY_BANNER_OPTIONS;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Logger.logMethod(TAG, "onCreate");
         EventBus.getDefault().register(this);
-
 
     }
 
@@ -113,7 +130,7 @@ public class HomePageHotFragment extends BaseFragment implements Callback {
         mViewHolder.mParentView = parentView;
         mViewHolder.mView = LayoutInflater.from(mContext).inflate(
                 R.layout.fragment_homepage_hot, parentView, true);
-        mViewHolder.mPhotoListView = (PullToRefreshExpandableListView) mViewHolder.mView
+        mViewHolder.mPhotoListView = (PullToRefreshListView) mViewHolder.mView
                 .findViewById(R.id.fragment_homepage_hot_lv);
 
         mFollowListFooter = LayoutInflater.from(mContext).inflate(
@@ -123,9 +140,8 @@ public class HomePageHotFragment extends BaseFragment implements Callback {
         mFollowListFooter.setVisibility(View.GONE);
 
         mHotPhotoItems = new ArrayList<PhotoItem>();
-        mAdapter = new HomePageHotAdapter(mContext,
-                PhotoItemView.PhotoListType.HOT_FOCUS_REPLY, mHotPhotoItems,
-                bannerList);
+        mAdapter = new PhotoListAdapter(mContext,
+                PhotoItemView.PhotoListType.HOT_FOCUS_ASK, mHotPhotoItems);
         mViewHolder.mPhotoListView.getRefreshableView().setAdapter(mAdapter);
 
         mListener = new PhotoListListener(mContext);
@@ -140,21 +156,6 @@ public class HomePageHotFragment extends BaseFragment implements Callback {
         }
 
         mViewHolder.mPhotoListView.setScrollingWhileRefreshingEnabled(true);
-
-        int groupCount = mAdapter.getGroupCount();
-        for (int ix = 0; ix < groupCount; ++ix) {
-            mViewHolder.mPhotoListView.getRefreshableView().expandGroup(ix);
-        }
-
-        // 设置点击分组不可展开
-        mViewHolder.mPhotoListView.getRefreshableView()
-                .setOnGroupClickListener(new OnGroupClickListener() {
-                    @Override
-                    public boolean onGroupClick(ExpandableListView parent,
-                                                View v, int groupPosition, long id) {
-                        return true;
-                    }
-                });
 
         // TODO 检测耗时
         try {
@@ -173,6 +174,157 @@ public class HomePageHotFragment extends BaseFragment implements Callback {
         return parentView;
     }
 
+    public View getBannerView () {
+        bannerView = LayoutInflater.from(mContext).inflate(
+                R.layout.homepage_hot_banner_view, null);
+
+        mScrollLayout = (LinearLayout) bannerView.findViewById(R.id.scroll_layout);
+
+        mBannerViewPager = (AutoScrollViewPager) bannerView
+                .findViewById(R.id.hot_banner_viewpager);
+        LinearLayout.LayoutParams scrollLayoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+
+        for (int i = 0,length = mBannerItems.size(); i < length ; i++) {
+            ImageView scrollImage = new ImageView(mContext);
+            scrollImage.setScaleType(ImageView.ScaleType.MATRIX);
+            if (i == 0) {
+                scrollImage.setBackgroundResource(R.drawable.shape_scroll_banner_select);
+                scrollLayoutParams.setMargins(0,0,0,0);
+            } else {
+                scrollImage.setBackgroundResource(R.drawable.shape_scroll_banner_unselect);
+                scrollLayoutParams.setMargins(Utils.dipToPx(mContext, 5),0,0,0);
+            }
+            scrollImage.setLayoutParams(scrollLayoutParams);
+
+            mScrollLayout.addView(scrollImage);
+            mScrollImages.add(scrollImage);
+
+        }
+
+        if (mBannerViewPager != null) {
+            mBannerViewPager.setOnPageChangeListener(null);
+        }
+
+        initAdapter();
+        mBannerViewPager.setOnPageChangeListener(bannerListener);
+        mBannerViewPager.setInterval(3000);  // 设置自动滚动的间隔时间，单位为毫秒
+        mBannerViewPager.startAutoScroll();  // 启动自动滚动
+
+        return bannerView;
+    }
+
+    public class BannerOnPageChangeListener implements ViewPager.OnPageChangeListener {
+        @Override
+        public void onPageSelected(int page) {
+            for (int i = 0 ,length = mBannerItems.size(); i < length; i++) {
+                if (page == i) {
+                    mScrollImages.get(i).setBackgroundResource(R.drawable.shape_scroll_banner_select);
+                } else {
+                    mScrollImages.get(i).setBackgroundResource(R.drawable.shape_scroll_banner_unselect);
+                }
+            }
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int arg0) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void onPageScrolled(int arg0, float arg1, int arg2) {
+            // TODO Auto-generated method stub
+
+        }
+    }
+
+    private void initAdapter() {
+        // viewpager滚动页面
+        RelativeLayout.LayoutParams imageParams = new RelativeLayout.LayoutParams(
+                android.view.ViewGroup.LayoutParams.MATCH_PARENT,
+                Utils.dipToPx(mContext, 166));
+
+        PsGodImageLoader imageLoader = PsGodImageLoader.getInstance();
+        final ArrayList<View> bannerListViews = new ArrayList<View>();
+        for (int i = 0 ,length = mBannerItems.size(); i < length; i++) {
+
+            final BannerData bannerData = mBannerItems.get(i);
+            ImageView bannerImage = new ImageView(mContext);
+            bannerImage.setLayoutParams(imageParams);
+            bannerImage.setScaleType(ImageView.ScaleType.FIT_XY);
+            imageLoader.displayImage(bannerData.getSmall_pic(), bannerImage, mOptions);
+            bannerListViews.add(bannerImage);
+
+            bannerImage.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View arg0) {
+                    if (bannerData.getUrl().indexOf("http") != -1) {
+                        Intent intent = new Intent(mContext,
+                                WebBrowserActivity.class);
+                        intent.putExtra(WebBrowserActivity.KEY_URL, bannerData.getUrl());
+                        intent.putExtra(WebBrowserActivity.KEY_DESC, bannerData.getDesc());
+                        mContext.startActivity(intent);
+                    } else if (bannerData.getUrl().indexOf("tupppai://") == -1) {
+                        Intent intent = new Intent(mContext,
+                                WebBrowserActivity.class);
+                        intent.putExtra(WebBrowserActivity.KEY_URL,
+                                BaseRequest.PSGOD_BASE_URL + bannerData.getUrl());
+                        intent.putExtra(WebBrowserActivity.KEY_DESC, bannerData.getDesc());
+                        mContext.startActivity(intent);
+                    } else {
+                        String[] s = bannerData.getUrl().split("tupppai://");
+                        if (s.length == 2) {
+                            String[] thumb = s[1].split("/");
+                            if (thumb.length == 2) {
+                                Intent intent = new Intent();
+                                if (thumb[0].equals("activity")) {
+                                    intent.setClass(mContext, RecentActActivity.class);
+                                    intent.putExtra(RecentActActivity.INTENT_ID, thumb[1]);
+                                } else {
+                                    intent.setClass(mContext, ChannelActivity.class);
+                                    intent.putExtra(ChannelActivity.INTENT_ID, thumb[1]);
+                                    intent.putExtra(ChannelActivity.INTENT_TITLE, bannerData.getDesc());
+                                }
+                                mContext.startActivity(intent);
+                            }
+                        }
+                    }
+                }
+            });
+
+        }
+
+        PagerAdapter mPagerAdapter = new PagerAdapter() {
+
+            @Override
+            public boolean isViewFromObject(View arg0, Object arg1) {
+                return arg0 == arg1;
+            }
+
+            @Override
+            public int getCount() {
+                return bannerListViews.size();
+            }
+
+            @Override
+            public void destroyItem(View container, int position, Object object) {
+                ((AutoScrollViewPager) container).removeView(bannerListViews
+                        .get(position));
+            }
+
+            @Override
+            public Object instantiateItem(View container, int position) {
+                ((AutoScrollViewPager) container).addView(bannerListViews
+                        .get(position));
+                return bannerListViews.get(position);
+            }
+        };
+
+        mBannerViewPager.setAdapter(mPagerAdapter);
+    }
+
     private void loadBannerData() {
         HomePageGetBannerRequest.Builder builder = new HomePageGetBannerRequest.Builder()
                 .setListener(mBannerListener).setErrorListener(
@@ -187,8 +339,10 @@ public class HomePageHotFragment extends BaseFragment implements Callback {
     private Listener<List<BannerData>> mBannerListener = new Listener<List<BannerData>>() {
         @Override
         public void onResponse(List<BannerData> bannerItems) {
-            bannerList.addAll(bannerItems);
-            mAdapter.notifyDataSetChanged();
+            mBannerItems.addAll(bannerItems);
+            if (bannerItems.size() > 0) {
+                mViewHolder.mPhotoListView.getRefreshableView().addHeaderView(getBannerView());
+            }
         }
     };
 
@@ -408,12 +562,6 @@ public class HomePageHotFragment extends BaseFragment implements Callback {
 
             mViewHolder.mPhotoListView.onRefreshComplete();
 
-            // 展开所有分组
-            int groupCount = mAdapter.getGroupCount();
-            for (int ix = 0; ix < groupCount; ++ix) {
-                mViewHolder.mPhotoListView.getRefreshableView().expandGroup(ix);
-            }
-
             PhotoItem.savePhotoList(getActivity(), mPhotoItemDao, items,
                     PhotoItem.TYPE_HOME_HOT);
 
@@ -447,7 +595,7 @@ public class HomePageHotFragment extends BaseFragment implements Callback {
     private static class ViewHolder {
         ViewGroup mParentView;
         View mView;
-        PullToRefreshExpandableListView mPhotoListView;
+        PullToRefreshListView mPhotoListView;
         RelativeLayout mParent;
     }
 
