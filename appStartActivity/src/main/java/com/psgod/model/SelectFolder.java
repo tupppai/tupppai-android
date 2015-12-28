16f96015d2071ce91546b5e817f1b19d747d6589
pires <1764840217@qq.com>
2015-12-28 19:05:45 +0800

    图片选择

---
+++ appStartActivity/src/main/java/com/psgod/model/SelectFolder.java
@@ -0,0 +1,26 @@
+package com.psgod.model;
+
+import android.media.Image;
+
+import java.util.List;
+
+/**
+ * Created by pires on 15/12/28.
+ */
+public class SelectFolder {
+    public String name;
+    public String path;
+    public SelectImage cover;
+    public List<SelectImage> images;
+
+    @Override
+    public boolean equals(Object o) {
+        try {
+            SelectFolder other = (SelectFolder) o;
+            return this.path.equalsIgnoreCase(other.path);
+        }catch (ClassCastException e){
+            e.printStackTrace();
+        }
+        return super.equals(o);
+    }
+}
